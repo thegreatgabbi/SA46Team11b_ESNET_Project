5981b10dd752ffbad78ee8ec04f2eacff4eee307
Palaniappan Vallikannu <e0302271@u.nus.edu>
2018-03-23 10:57:52 +0800

    Completed Make Booking Form.

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -10,11 +10,37 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class MakeaBookingForm : Form
     {
-        public Form1()
+        public MakeaBookingForm()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LookUpBtn_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Okbtn_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
