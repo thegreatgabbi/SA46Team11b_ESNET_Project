21087825be6e54974d739b664a99a855459a0e56
weinchester <weinchester@gmail.com>
2018-03-26 15:58:39 +0800

    Test successful for tabl controls

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,8 +12,7 @@ namespace WindowsFormsApp1
 {
     public partial class WelcomeForm : Form
     {
-        child1 f1 = new child1();
-        child2 f2 = new child2();
+       
 
         public WelcomeForm()
         {
@@ -22,24 +21,20 @@ namespace WindowsFormsApp1
 
         private void bookFacilityToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            f2.Hide();
-            f1.Show();
-           // f1.MdiParent = this;
+           
             
         }
 
         private void memberInformationToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            f1.Hide();
-           // f2.MdiParent = this;
-            f2.Show();
+          
         }
 
         private void WelcomeForm_Load(object sender, EventArgs e)
         {
             var f2 = new child1();
             f2.TopLevel = false;
-            f2.Location = new Point(5, 5);
+            f2.Location = new Point(0, 0);
             f2.FormBorderStyle = FormBorderStyle.None;
             f2.Visible = true;
             this.tabPage1.Controls.Add(f2);
@@ -47,8 +42,7 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            f2.Hide();
-            f1.Show();
+           
            
         }
     }
